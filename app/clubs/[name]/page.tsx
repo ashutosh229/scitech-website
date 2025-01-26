7be83bc6c7b1b92cd78cfb9d061e69edbe733436
ashutosh229 <akumarjha875@gmail.com>
2025-01-27 01:58:44 +0530

    initial push for the club pages

--- app/clubs/[name]/page.tsx
+++ app/clubs/[name]/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { blogs } from "@/data/clubs";
+import { clubMembers } from "@/data/clubs";
 
 const ClubPage = ({ params: { name } }: { params: { name: string } }) => {
   return <div>{name}</div>;
