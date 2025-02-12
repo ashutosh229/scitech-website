0daa673a73344b314f95b44a341dc1ff647cd6fa
ashutosh229 <akumarjha875@gmail.com>
2025-02-12 23:52:38 +0530

    added

--- data/club_events/epsilon/data.ts
+++ data/club_events/epsilon/data.ts
@@ -1,4 +1,16 @@
-import { EventType } from "@/types/events";
+type EventType = {
+    title: string;
+    hosting: string;
+    club: string;
+    description: string;
+    date: string;
+    month: string;
+    year: string;
+    time: string;
+    location: string;
+    image: string;
+    isSingleEvent: boolean;
+};
 
 const events: EventType[] = [];
 
